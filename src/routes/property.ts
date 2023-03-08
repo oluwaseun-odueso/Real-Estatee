69f3410b25216e13546ba25a2498f43fe21922f4
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-03-08 07:39:27 +0100

    Get features

--- src/routes/property.ts
+++ src/routes/property.ts
@@ -6,13 +6,14 @@ import {
 } from '../controllers/property';
 
 import { 
-    addPropertyFeature 
+    addPropertyFeature, getPropertyFeatures 
 } from '../controllers/propertyFeatures';
 
 const router = express.Router()
 
 router.post('/put_property_for_sale', verifySellerToken, addProperty);
 router.post('/add_property_features', verifySellerToken, addPropertyFeature)
-router.get('/getProperty/:id', getProperty)
+router.get('/get_property/:id', getProperty)
+router.get('/get_property_features/:id', getPropertyFeatures)
 
 export default router;
